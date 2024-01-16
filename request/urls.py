2ac49bfa77eab678c18f271d2102b64f08cd49ea
noa_k <noa.work32@gmail.com>
2024-01-16 15:14:07 +0900

    テンプレートの追加

---
+++ request/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'request'
+
+urlpatterns = [
+  path('', views.index, name='index'),
+  path('detail/', views.detall, name='detail')
+]
+
